be02f3b7cbd8e86dd1c6edd17b3f6df31354493c
EstefanoCarbajal <tefa3099@hotmail.com>
2021-04-14 08:59:10 -0500

    Update linkedlist.h
    linked list

--- linkedlist.h
+++ linkedlist.h
@@ -5,21 +5,44 @@
 // #include <vector>
 // using namespace std;
 
+//creado por estefano carbajal
+
 template <typename T>
+class Node {
+    public:
+        int data;
+    Node * next;
+};
+// Retorna el elemento al comienzo
+  T front(Node * n) {
+
+    while (n != NULL) {
+        cout << n->data
+        n = n->next;}}
+        // Agrega un elemento al comienzo
+  void push_front(struct Node ** head, T &elem) {
+     struct Node * new_node = (struct Node *);
+    Node * back = * head;
+    new_node->data = element;
+    new_node->next = NULL;
+    while (back->next != NULL)
+        back = back->next;
+    back->next = new_node;
+
 class LinkedList
 {
   private:
   // members here
   public:
-    T front(); // Retorna el elemento al comienzo
+  (); // Retorna el elemento al comienzo
     T back(); // Retorna el elemento al final
-    void push_front(T &elem); // Agrega un elemento al comienzo 
+     // Agrega un elemento al comienzo
     void push_back(T &elem); // Agrega un elemento al final
     void pop_front(); // Remueve el elemento al comienzo pero no lo retorna
     void pop_back(); // Remueve el elemento al final pero no lo retorna
     T operator[](size_t pos); // Retorna el elemento en la posición indicada
     bool empty(); // Retorna si la lista está vacía o no
-    size_t size(); // Retorna el tamaño de la lista 
+    size_t size(); // Retorna el tamaño de la lista
     void clear(); // Elimina todos los elementos de la lista
     void sort(); // Ordena la lista
     void reverse(); // Revierte la lista
